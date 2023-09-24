3e8475472e89eada1be75315412cf3bb5c2c04f4
sl1pper <averin_sd@icloud.com>
2023-09-24 23:12:45 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "str_easy.h"
+using namespace std;
+
+int main() {
+    cout << itc_reverse_str("Hello, world!");
+    return 0;
+}
---
+++ str_easy.h
@@ -0,0 +1,16 @@
+#ifndef STREASY_STR_EASY_H
+#define STREASY_STR_EASY_H
+
+#include <string>
+using namespace std;
+
+string itc_hello_str(string name); // 1
+long long itc_len(string str); // 2
+void itc_print_copy_str(string str, int number); // 3
+void itc_first_end_three(string str); // 4
+int itc_count_char_in_str(char ch, string str); // 5
+string itc_even_place(string str); // 6
+double itc_percent_lower_uppercase(string str); // 7
+string itc_reverse_str(string str); // 8
+
+#endif //STREASY_STR_EASY_H
